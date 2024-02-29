b73cb4f1e5c7f62d93a9348548d1b711df19856d
Bartlomiej-cmd <molotov@op.pl>
2024-02-29 12:55:01 +0100

    aktualizacja 2 mój  projekt

--- CRM_Ubezpieczenia/ubezpieczenia_app/views.py
+++ CRM_Ubezpieczenia/ubezpieczenia_app/views.py
@@ -57,58 +57,58 @@ def agent_dashboard(request):
     return render(request, 'agent_dashboard.html', {'clients': clients, 'policies': policies, 'cars': cars})
 
 
-@login_required
-def list_cars(request):
-    cars = Car.objects.all()
-    return render(request, 'list_cars.html', {'cars': cars})
+# @login_required
+# def list_cars(request):
+#     cars = Car.objects.all()
+#     return render(request, 'list_cars.html', {'cars': cars})
 
 
-@login_required
-def list_policies(request):
-    policies = Policy.objects.all()
-    return render(request, 'list_policies.html', {'policies': policies})
-
-
-@login_required
-def list_clients(request):
-    clients = Client.objects.all()
-    return render(request, 'list_clients.html', {'clients': clients})
-
-
-@login_required
-def add_car(request):
-    if request.method == 'POST':
-        form = CarForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('database_view')
-    else:
-        form = CarForm()
-    return render(request, 'add_car.html', {'form': form})
-
-
-@login_required
-def add_client(request):
-    if request.method == 'POST':
-        form = ClientForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('client_list')
-    else:
-        form = ClientForm()
-    return render(request, 'add_client.html', {'form': form})
-
-
-@login_required
-def add_policy(request):
-    if request.method == 'POST':
-        form = PolicyForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('policy_list')
-    else:
-        form = PolicyForm()
-    return render(request, 'add_policy.html', {'form': form})
+# @login_required
+# def list_policies(request):
+#     policies = Policy.objects.all()
+#     return render(request, 'list_policies.html', {'policies': policies})
+#
+#
+# @login_required
+# def list_clients(request):
+#     clients = Client.objects.all()
+#     return render(request, 'list_clients.html', {'clients': clients})
+#
+#
+# @login_required
+# def add_car(request):
+#     if request.method == 'POST':
+#         form = CarForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('database_view')
+#     else:
+#         form = CarForm()
+#     return render(request, 'add_car.html', {'form': form})
+#
+#
+# @login_required
+# def add_client(request):
+#     if request.method == 'POST':
+#         form = ClientForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('client_list')
+#     else:
+#         form = ClientForm()
+#     return render(request, 'add_client.html', {'form': form})
+#
+#
+# @login_required
+# def add_policy(request):
+#     if request.method == 'POST':
+#         form = PolicyForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('policy_list')
+#     else:
+#         form = PolicyForm()
+#     return render(request, 'add_policy.html', {'form': form})
 
 
 class CarListView(ListView):
